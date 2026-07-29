5106748da7a476a70be6e1c0a141f010338feb33
Rohit Choudhary <rohitchoudhary8037@gmail.com>
2026-07-29 18:48:50 +0530

    Updating DQN evaluating pipeline

--- evaluate_dqn.py
+++ evaluate_dqn.py
@@ -9,3 +9,14 @@ from agents.baseline import (
 )
 
 from agents.dqn_agent import DQNAgent
+env = DynamicPricingEnv()
+
+dqn_agent = DQNAgent(
+    state_size=2,
+    action_size=env.action_space.n
+)
+
+dqn_agent.load_model("models/dqn_model.pth")
+
+# Evaluation mode
+dqn_agent.epsilon = 0
