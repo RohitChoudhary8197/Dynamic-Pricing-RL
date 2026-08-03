9ea0d782adbb461ebe3b76800f48b73f8d7798e3
Rohit Choudhary <rohitchoudhary8037@gmail.com>
2026-08-03 20:43:10 +0530

    latest commit

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-
     return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    inventory = request.form["inventory"]
+    days_left = request.form["days_left"]
+
+    return render_template(
+        "result.html",
+        inventory=inventory,
+        days_left=days_left,
+        price="₹4500",
+        revenue="₹425000"
+    )
 
 
 @app.route("/about")
 def about():
-
     return render_template("about.html")
 
 
-if __name__ == "__main__":
+@app.route("/analytics")
+def analytics():
+    return render_template("analytics.html")
 
-    app.run(
-        debug=True
-    )
-    
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
